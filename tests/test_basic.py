a074549d9f1b443cf5b7bd8d78635f7455575551
simomarsili <simomarsili@gmail.com>
2017-09-20 13:28:40 +0200

    switch to pytest

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,42 +1,24 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import,division,print_function,unicode_literals
-from builtins import *
-import unittest
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import (  # pylint: disable=redefined-builtin, unused-import
+    bytes, dict, int, list, object, range, str,
+    ascii, chr, hex, input, next, oct, open,
+    pow, round, super,
+    filter, map, zip)
 import ndd
+import pytest
 import numpy as np
- 
-class TestNdd(unittest.TestCase):
- 
-    def test_1_100_100(self):
-        a=1.0; ns=100; nd=100
-        result = np.float64(4.322717746281727)
-        np.random.seed(123)
-        pp=np.random.dirichlet([a]*ns)
-        data = np.random.multinomial(nd,pp)
-        self.assertEqual(ndd.entropy(data), result)
 
-    def test_01_100_100(self):
-        a=0.1; ns=100; nd=100
-        result = np.float64(2.569415088680637)
-        np.random.seed(123)
-        pp=np.random.dirichlet([a]*ns)
-        data = np.random.multinomial(nd,pp)
-        self.assertEqual(ndd.entropy(data), result)
+tests = [
+    (0.01, 100, 100, 0.21622160227928622),
+    (0.1, 100, 100, 2.569415088680637),
+    (1.0, 100, 100, 4.322717746281727)]
 
-    def test_001_100_100(self):
-        a=0.01; ns=100; nd=100
-        result = np.float64(0.21622160227928622)
-        np.random.seed(123)
-        pp=np.random.dirichlet([a]*ns)
-        data = np.random.multinomial(nd,pp)
-        self.assertEqual(ndd.entropy(data), result)
-
-    def test_histogram(self):
-        np.random.seed(123)
-        data = np.random.randint(1,11,1000)
-        _, h = np.unique(data, return_counts=True)
-        self.assertEqual(ndd.histogram(data), list(h))
-
-if __name__ == '__main__':
-    unittest.main()
-
+@pytest.mark.parametrize('a, ns, nd, result', tests)
+def test_basic(a, ns, nd, result):
+    """Basic tests."""
+    np.random.seed(123)
+    pp=np.random.dirichlet([a]*ns)
+    data = np.random.multinomial(nd,pp)
+    assert ndd.entropy(data) == np.float64(result)
