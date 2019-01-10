dbbfc64da736ac3233913f86c12c5a0e2dd207dd
simomarsili <simomarsili@gmail.com>
2019-01-10 16:44:28 +0100

    BaseEstimator: add set_params()

--- ndd/base.py
+++ ndd/base.py
@@ -11,6 +11,7 @@ from builtins import (  # pylint: disable=redefined-builtin, unused-import
     pow, round, super,
     filter, map, zip)
 from inspect import signature
+from collections import defaultdict
 
 
 class BaseEstimator(object):
@@ -80,3 +81,38 @@ class BaseEstimator(object):
                 out.update((key + '__' + k, val) for k, val in deep_items)
             out[key] = value
         return out
+
+    def set_params(self, **params):
+        """Set the parameters of this estimator.
+        The method works on simple estimators as well as on nested objects
+        (such as pipelines). The latter have parameters of the form
+        ``<component>__<parameter>`` so that it's possible to update each
+        component of a nested object.
+        Returns
+        -------
+        self
+        """
+        if not params:
+            # Simple optimization to gain speed (inspect is slow)
+            return self
+        valid_params = self.get_params(deep=True)
+
+        nested_params = defaultdict(dict)  # grouped by prefix
+        for key, value in params.items():
+            key, delim, sub_key = key.partition('__')
+            if key not in valid_params:
+                raise ValueError('Invalid parameter %s for estimator %s. '
+                                 'Check the list of available parameters '
+                                 'with `estimator.get_params().keys()`.' %
+                                 (key, self))
+
+            if delim:
+                nested_params[key][sub_key] = value
+            else:
+                setattr(self, key, value)
+                valid_params[key] = value
+
+        for key, sub_params in nested_params.items():
+            valid_params[key].set_params(**sub_params)
+
+        return self
