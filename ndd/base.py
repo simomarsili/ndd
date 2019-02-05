4ef698be8347c7a6f235b4722f1d26d852474e74
simomarsili <simomarsili@gmail.com>
2019-02-05 09:45:50 +0100

    move BaseEstimator to base_estimator module with sklearn license

--- ndd/base.py
+++ ndd/base.py
@@ -2,153 +2,15 @@
 # Copyright (C) 2016,2017 Simone Marsili
 # All rights reserved.
 # License: BSD 3 clause
-"""Base classes module."""
+"""Base EntropyEstimator class."""
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from builtins import (  # pylint: disable=redefined-builtin, unused-import
     bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next,
     oct, open, pow, round, super, filter, map, zip)
 import numpy
-try:
-    from inspect import signature  # pylint: disable=wrong-import-order
-except ImportError:
-    from ndd.funcsigs import signature
-from collections import defaultdict  # pylint: disable=wrong-import-order
+from ndd.base_estimator import BaseEstimator
 import ndd.fnsb
-
-
-class BaseEstimator(object):
-    """Base class for estimators from sklearn.
-
-    The class is consistent with sklearn estimator API:
-
-    All estimator objects expose a ``fit`` method that takes a dataset
-    (usually a 2-d array):
-
-    >>> estimator.fit(data)
-
-    **Estimator parameters**: All the parameters of an estimator can be set
-    when it is instantiated or by modifying the corresponding attribute::
-
-    >>> estimator = Estimator(param1=1, param2=2)
-    >>> estimator.param1
-    1
-
-    **Estimated parameters**: When data is fitted with an estimator,
-    parameters are estimated from the data at hand. All the estimated
-    parameters are attributes of the estimator object ending by an
-    underscore::
-
-    >>> estimator.estimated_param_ #doctest: +SKIP
-    """
-
-    @classmethod
-    def _get_param_names(cls):
-        """Get parameter names for the estimator"""
-        # fetch the constructor or the original constructor before
-        # deprecation wrapping if any
-        init = getattr(cls.__init__, 'deprecated_original', cls.__init__)
-        if init is object.__init__:
-            # No explicit constructor to introspect
-            return []
-
-        # introspect the constructor arguments to find the model parameters
-        # to represent
-        init_signature = signature(init)
-        # Consider the constructor parameters excluding 'self'
-        parameters = [
-            p for p in init_signature.parameters.values()
-            if p.name != 'self' and p.kind != p.VAR_KEYWORD
-        ]
-        for p in parameters:
-            if p.kind == p.VAR_POSITIONAL:
-                raise RuntimeError(
-                    "scikit-learn estimators should always "
-                    "specify their parameters in the signature"
-                    " of their __init__ (no varargs)."
-                    " %s with constructor %s doesn't "
-                    " follow this convention." % (cls, init_signature))
-        # Extract and sort argument names excluding 'self'
-        return sorted([p.name for p in parameters])
-
-    def get_params(self, deep=True):
-        """Get parameters for this estimator.
-        Parameters
-        ----------
-        deep : boolean, optional
-            If True, will return the parameters for this estimator and
-            contained subobjects that are estimators.
-        Returns
-        -------
-        params : mapping of string to any
-            Parameter names mapped to their values.
-        """
-        out = dict()
-        for key in self._get_param_names():
-            value = getattr(self, key, None)
-            if deep and hasattr(value, 'get_params'):
-                deep_items = value.get_params().items()
-                out.update((key + '__' + k, val) for k, val in deep_items)
-            out[key] = value
-        return out
-
-    def set_params(self, **params):
-        """Set the parameters of this estimator.
-        The method works on simple estimators as well as on nested objects
-        (such as pipelines). The latter have parameters of the form
-        ``<component>__<parameter>`` so that it's possible to update each
-        component of a nested object.
-        Returns
-        -------
-        self
-        """
-        if not params:
-            # Simple optimization to gain speed (inspect is slow)
-            return self
-        valid_params = self.get_params(deep=True)
-
-        nested_params = defaultdict(dict)  # grouped by prefix
-        for key, value in params.items():
-            key, delim, sub_key = key.partition('__')
-            if key not in valid_params:
-                raise ValueError(
-                    'Invalid parameter %s for estimator %s. '
-                    'Check the list of available parameters '
-                    'with `estimator.get_params().keys()`.' % (key, self))
-
-            if delim:
-                nested_params[key][sub_key] = value
-            else:
-                setattr(self, key, value)
-                valid_params[key] = value
-
-        for key, sub_params in nested_params.items():
-            valid_params[key].set_params(**sub_params)
-
-        return self
-
-    def __repr__(self):
-        class_name = self.__class__.__name__
-        return '%s(%s)' % (
-            class_name,
-            _pprint(
-                self.get_params(deep=False),
-                offset=len(class_name),
-            ),
-        )
-
-    def __getstate__(self):
-        try:
-            state = super().__getstate__()
-        except AttributeError:
-            state = self.__dict__.copy()
-        return state
-
-    def __setstate__(self, state):
-        try:
-            super().__setstate__(state)
-        except AttributeError:
-            self.__dict__.update(state)
 
 
 class EntropyEstimatorMixin(object):
