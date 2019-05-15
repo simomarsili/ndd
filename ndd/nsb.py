14833dfffb14ccd83de681de0c5060fede63b70e
simomarsili <simo.marsili@gmail.com>
2019-05-15 17:55:22 +0200

    minor style

--- ndd/nsb.py
+++ ndd/nsb.py
@@ -135,15 +135,6 @@ def jensen_shannon_divergence(pk, k=None, alpha=None, plugin=False):
     return js
 
 
-def _nbins(data):
-    """
-    The number of unique elements along axis 0. If data is p-dimensional,
-    the num. of unique elements for each variable.
-    """
-    # reshape as a p-by-n array
-    return [len(numpy.unique(v)) for v in data]
-
-
 def histogram(data, axis=1, r=None):
     """Compute an histogram from a data array. Wrapper to numpy.unique.
 
@@ -451,6 +442,15 @@ def conditional_entropy(ar, c, ks=None, axis=1, r=None):
 
     counts = histogram(ar)
     return estimator(counts, k=ks) - hc
+
+
+def _nbins(data):
+    """
+    The number of unique elements along axis 0. If data is p-dimensional,
+    the num. of unique elements for each variable.
+    """
+    # reshape as a p-by-n array
+    return [len(numpy.unique(v)) for v in data]
 
 
 def _check_input_data(ar):
