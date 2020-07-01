6195473e9987018dc74a08817e2475b75833230d
simomarsili <simo.marsili@gmail.com>
2020-07-01 08:31:55 +0200

    minor fixes

--- utils/bars.py
+++ utils/bars.py
@@ -10,11 +10,10 @@ import figs
 import ndd
 import ndd.estimators
 
-N = 10
+N = 1000
 P = 1000000
 ALPHA = 0.001
-NITER = 10
-R = 100
+R = 50
 
 numpy.random.seed(1234)
 
@@ -58,43 +57,60 @@ def entropy_estimate(a=None, r=1):
         true = []
         ests = []
         err = []
+        bounds = []
         for _ in range(r):
-            S0, x, y = entropy_estimate(a=a)
+            S0, x, y, b = entropy_estimate(a=a)
             true.append(S0)
             ests.append(x)
             err.append(y)
-        return numpy.array(true), numpy.array(ests).T, numpy.array(err).T
+            bounds.append(b)
+        return (numpy.array(true), numpy.array(ests).T, numpy.array(err).T,
+                numpy.array(bounds))
     c, S0 = random_counts(a=a)
     S = [None] * 3
     err = [None] * 3
     S[0], err[0] = entropy(c) / numpy.log(P), None
     S[1], err[1] = ndd.entropy(c, return_std=1) / numpy.log(P)
+    entropy_estimate.bounded = ndd.entropy.bounded
     S[2], err[2] = ndd.entropy(c, k=P, return_std=1) / numpy.log(P)
-    return S0, tuple(S), tuple(err)
+    return S0, tuple(S), tuple(err), entropy_estimate.bounded
 
 
-def plot_errorbar(axes, x, y, err, label=None):
+def plot_errorbar(axes, x, y, err, c=None):
+    # pylint: disable=invalid-unary-operand-type
     """Plotter"""
     kwargs = {
-        # 'markersize': 4,
         'alpha': 0.5,
         'elinewidth': 1,
         'capsize': 3,
         'errorevery': 1,
         'ls': '',
     }
+
+    if c is None:
+        c = list(range(len(x)))
+
     if None in err:
-        axes.scatter(x, y, zorder=1, label=label, marker='o', alpha=0.5)
+        axes.scatter(x[c], y[c], zorder=1, marker='o', alpha=0.5)
+        axes.scatter(x[~c], y[~c], zorder=1, marker='x', alpha=0.5, c='teal')
         return axes
-    axes.errorbar(x, y, **kwargs, yerr=err, zorder=1, label=label, marker='o')
-    axes.plot(x, x, '--', lw=1, zorder=2, color='black', alpha=0.5)
+
+    axes.errorbar(x[c], y[c], **kwargs, yerr=err[c], zorder=1, marker='o')
+    axes.errorbar(x[~c],
+                  y[~c],
+                  **kwargs,
+                  yerr=err[~c],
+                  zorder=1,
+                  marker='x',
+                  c='teal')
+    axes.plot(x, x, '--', lw=1, zorder=2, c='black', alpha=0.5)
     return axes
 
 
 if __name__ == '__main__':
     ylim = (-0.2, 1.2)
 
-    xs, ys, errs = entropy_estimate(r=R)
+    xs, ys, errs, bounded = entropy_estimate(r=R)
 
     titles = [
         'scipy.stats.entropy(counts)', 'ndd.entropy(counts)',
@@ -103,11 +119,11 @@ if __name__ == '__main__':
     xlabels = [None, 'entropy', None]
     ylabels = ['entropy estimate'] + [None] * 2
 
-    fonts = figs.TalkFonts
+    fonts = figs.PosterFonts
     fonts['axes.titlesize'] = 'small'
     with mpl.style.context([figs.Custom, figs.FigSizeL, fonts],
                            after_reset=True):
-        fig, axs = plt.subplots(1, 3, figsize=figs.fig_size(5, 3))
+        fig, axs = plt.subplots(1, 3, figsize=figs.fig_size(16, 6))
 
         for j, title in enumerate(titles):
             with figs.axes(
@@ -117,6 +133,8 @@ if __name__ == '__main__':
                     ylim=ylim,
                     xlabel=xlabels[j],
                     ylabel=ylabels[j],
-            ) as ax:
-                plot_errorbar(ax, xs, ys[j], errs[j])
+                    # x_invisible=j != 1,
+                    y_invisible=j != 0) as ax:
+                plot_errorbar(ax, xs, ys[j], errs[j], c=bounded)
         plt.show()
+        fig.savefig('bars.svg', format='svg')
