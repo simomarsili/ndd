4a9c3d5c0e7c44aad90873434068178d3f887c89
simomarsili <simo.marsili@gmail.com>
2020-09-23 17:41:24 +0200

    compute_estimate

--- ndd/entry.py
+++ ndd/entry.py
@@ -18,7 +18,7 @@ def parse_args():
     parser.add_argument('-i',
                         '--input_file',
                         type=str,
-                        help='Input file; defaults to stdin.',
+                        help='JSON input file; defaults to stdin.',
                         default=None)
     parser.add_argument('-o',
                         '--output_file',
@@ -44,6 +44,37 @@ def parse_args():
     return parser.parse_args()
 
 
+def compute_estimate(data, k=None, estimator=None, alpha=None):
+    """Entropy estimation given data."""
+
+    zk = None
+    if isinstance(data, dict) and 'nk' in data:
+        nk = data['nk']
+        k = k or data.get('k', None)
+        zk = data.get('zk', None)
+    else:
+        # list of integers counts or mapping to ints
+        nk = data
+
+    if estimator is not None:
+        try:
+            algorithm = ndd.entropy_estimators[estimator]
+        except KeyError:
+            raise ValueError('Invalid estimator. Available estimators:\n'
+                             '%s' % available_estimators)
+        if alpha is not None:  # use alpha only if the estimator is given
+            try:
+                estimator = algorithm(alpha=alpha)
+            except TypeError:
+                estimator = algorithm()
+    else:
+        estimator = None
+
+    _ = ndd.entropy(nk, k=k, zk=zk, estimator=estimator)
+
+    return ndd.entropy.info
+
+
 def main():
     """ndd script."""
 
@@ -55,37 +86,16 @@ def main():
         with open(Path(args.input_file), 'r') as fp:
             data = json.load(fp)
 
-    k = args.alphabet_size
-    zk = None
-    if isinstance(data, dict) and 'nk' in data:
-        nk = data['nk']
-        k = k or data.get('k', None)
-        zk = data.get('zk', None)
-    else:
-        # list of integers counts or mapping to ints
-        nk = data
-
-    if args.estimator is not None:
-        try:
-            algorithm = ndd.entropy_estimators[args.estimator]
-        except KeyError:
-            raise ValueError('Invalid estimator. Available estimators:\n'
-                             '%s' % available_estimators)
-        if args.alpha is not None:  # use alpha only if the estimator is given
-            try:
-                estimator = algorithm(alpha=args.alpha)
-            except TypeError:
-                estimator = algorithm()
-    else:
-        estimator = None
-
-    _ = ndd.entropy(nk, k=k, zk=zk, estimator=estimator)
+    info = compute_estimate(data,
+                            k=args.alphabet_size,
+                            estimator=args.estimator,
+                            alpha=args.alpha)
 
     if args.output_file is None:
-        json.dump(ndd.entropy.info, sys.stdout, indent=4)
+        json.dump(info, sys.stdout, indent=4)
     else:
         with open(Path(args.output_file), 'w') as fp:
-            json.dump(ndd.entropy.info, fp, indent=4)
+            json.dump(info, fp, indent=4)
 
 
 if __name__ == '__main__':
